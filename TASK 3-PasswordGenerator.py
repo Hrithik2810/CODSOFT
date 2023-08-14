96687cc47d7afd8cf0c6566640cbc9170452a9b5
Hrithik2810 <139134264+Hrithik2810@users.noreply.github.com>
2023-08-14 21:11:19 +0530

    Update and rename PasswordGenerator.py to TASK 3-PasswordGenerator.py

--- TASK 3-PasswordGenerator.py
+++ TASK 3-PasswordGenerator.py
@@ -27,8 +27,9 @@ def reset_fields():
 # main window
 root = tk.Tk()
 root.title("Password Generator")
-root.geometry("650x350")
+root.geometry("650x350+300+100")
 root.config(background="Purple")
+root.resizable(0,0)
 
 # Title of the window
 window_title = tk.Label(root, text="Password Generator",font=("Ink Free",20,"bold"),fg="Black",background="Purple")
@@ -37,14 +38,14 @@ window_title.grid(row=0,column=1,pady=9)
 # Username input
 username_label = tk.Label(root, text="Username:",font=("Comic Sans MS",14),background="Purple",fg="White")
 username_label.grid(row=1, column=0)
-username_entry = tk.Entry(root, font=("Comic Sans MS",14))
+username_entry = tk.Entry(root,justify="center",font=("Comic Sans MS",14),highlightcolor="#7393B3")
 username_entry.grid(row=1,column=1,pady=5,sticky="Snew")
 
 
 # Password length input
 length_label = tk.Label(root, text="Password Length:",font=("Comic Sans MS",14),background="Purple",fg="White")
 length_label.grid(row=2, column=0)
-length_entry = tk.Entry(root,font=("Comic Sans MS",14))
+length_entry = tk.Entry(root,justify="center",font=("Comic Sans MS",14))
 length_entry.grid(row=2, column=1,pady=1)
 
 # Generated result pad
@@ -56,11 +57,11 @@ result_text = tk.Text(root, height=1, width=20, wrap=tk.WORD,font=("Comic Sans M
 result_text.grid(row=12, column=1,pady=1)
 
 # Generate button
-generate_button = tk.Button(root, text="Generate Password", command=generate_password,font=("Comic Sans MS",14))
+generate_button = tk.Button(root, cursor='hand2',text="Generate Password", command=generate_password,font=("Comic Sans MS",14))
 generate_button.grid(row=8, column=1,pady=10)
 
 # Reset button
-reset_button = tk.Button(root, text="Reset", command=reset_fields,font=("Comic Sans MS",14))
+reset_button = tk.Button(root, cursor="hand2",text="Reset", command=reset_fields,font=("Comic Sans MS",14))
 reset_button.grid(row=10, column=1,pady=10)
 
 root.mainloop()
